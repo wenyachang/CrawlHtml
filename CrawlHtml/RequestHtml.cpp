15667504776f4b04ea2a4144d100873f4b1ae56a
wenyachang <wenyachang@163.com>
2016-12-19 20:37:13 +0800

    RequestHtml

---
+++ CrawlHtml/RequestHtml.cpp
@@ -0,0 +1,31 @@
+#include "RequestHtml.h"
+
+RequestHtml* RequestHtml::_instance = NULL;
+
+RequestHtml::RequestHtml()
+{
+}
+
+RequestHtml::RequestHtml(QString url)
+{
+
+}
+
+
+RequestHtml::~RequestHtml()
+{
+}
+
+RequestHtml* RequestHtml::instance()
+{
+    if (_instance)
+    {
+        return new RequestHtml();
+    }
+    return _instance;
+}
+
+QString RequestHtml::getHtmlContent(QString url)
+{
+
+}
